94748540b16fe683d3ff6ec9952a6892f56999d9
ojosdegatonegro <diegofernandocubides@gmail.com>
2023-08-25 21:50:24 -0500

    add function that finds a free port and uses it to create a server. Modifies http-server for receive a port from input

--- 8.http-server.js
+++ 8.http-server.js
@@ -1,10 +1,15 @@
 const http = require('node:http');
+const { findAvailablePort } = require('./9.free-port.js');
+
+const desiredPort = process.argv[2] ?? '3000';
 
 const server = http.createServer((req, res) => {
   console.log('request received');
   res.end('response sent'); //termina la comunicacion
 });
 
-server.listen(3000, () => {
-  console.log('server listening on port 3000');
+findAvailablePort(desiredPort).then((port) => {
+  server.listen(port, () => {
+    console.log(`server listening on port http://localhost:${port}`);
+  });
 });
