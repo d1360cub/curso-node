79efbb06555221c658625f909cc2c1fa32b77b9a
ojosdegatonegro <diegofernandocubides@gmail.com>
2023-08-26 20:45:43 -0500

    http server with routes and status codes

---
+++ Week-2/1.http.js
@@ -0,0 +1,23 @@
+const http = require('node:http');
+
+const desiredPort = process.env.PORT ?? 1234;
+
+const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html', 'charset = utf - 8');
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Landing page');
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200;
+    res.end('Contacto');
+  } else {
+    res.statusCode = 404;
+    res.end('<h1>404</h1>');
+  }
+};
+
+const server = http.createServer(processRequest);
+
+server.listen(desiredPort, () => {
+  console.log(`server listening on port http://localhost:${desiredPort}`);
+});
